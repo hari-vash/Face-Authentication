2527af983cd2beec1dbffcf7438a620803e6d055
Hariansh Vashisht <harianshvashisht24155@gmail.com>
2025-09-12 01:14:05 +0530

    updated code

--- face_auth.py
+++ face_auth.py
@@ -3,6 +3,8 @@ import json
 from pathlib import Path
 import streamlit as st
 import cv2 as cv
+from PIL import Image
+
 
 def create_student_img_embedding(image_path):
     embedding_obj = DeepFace.represent(img_path=image_path)
@@ -18,30 +20,39 @@ def stored_embedding_retrieval(student_id):
     with path.open('r',encoding='utf-8') as f:
         data=json.load(f)
     idx = data["Student_ID"].index(student_id)
-    retrieved_embedding = data["img_embeddings"][idx]
+    retrieved_embedding = data["img_embedding"][idx]
     return retrieved_embedding
 
 st.set_page_config(page_title="Verify Student")
 st.title("Verify Student - Capture Selfie")
 st.write("Enter the Student ID and capture a selfie using your webcam.")
+frame_placeholder = st.empty()
+
 
 student_id = st.text_input("Enter your Student ID")
 
-if st.button("Take Selfie"):
-    if student_id is None:
-        st.warning("Please provide a Studnt ID")
-    else:
-        cap = cv.VideoCapture(0)
-        if not cap.isOpened():
-            st.error("Could not open webcam. Make sure your camera is available and not used by another app.")
-        else:
-            ret, frame = cap.read()
-            cap.release()
-            if not ret or frame is None:
-                st.error("Failed to capture image from camera")
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    st.error("Could not open webcam. Make sure your camera is available and not used by another app.")
+else:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.error("Failed to read frame from camera.")
+            break
+        frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        frame_placeholder.image(frame_rgb, channels="RGB")
+        
+        selfie_button = st.button("Take Selfie",key=None) 
+        
+        if selfie_button:
+            if student_id is None:
+                st.warning("Please provide a Student ID")
             else:
-                frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                cap.release()
                 if verify_student(create_student_img_embedding(frame_rgb), stored_embedding_retrieval(student_id)):
-                    print("Face matched")
+                    st.success("Face matched")
                 else:
-                    print("Face not recognized")
+                    st.error("Face not recognized")
+                    
+                    
